d1f9c578c1426e271b9c2d0fb050933cc805320d
beyzend <2965292+beyzend@users.noreply.github.com>
2020-07-16 01:22:48 +0000

    updated install_depds following whonix15 install guide.

--- install_deps.sh
+++ install_deps.sh
@@ -6,7 +6,7 @@ source ./common.sh
 step "Installing required dependencies."
 substep "Requesting sudo password to install packages."
 sudo apt-get update
-sudo apt-get install -y qemu-kvm libvirt-bin virt-manager libvirt-daemon-system libvirt-clients
-
+#sudo apt-get install -y qemu-kvm libvirt-bin virt-manager libvirt-daemon-system libvirt-clients
+sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients virt-manager gir1.2-spiceclientgtk-3.0 dnsmasq
 step "Done! You may also want to install and enable apparmor if you haven't already."
 step "The next step is 'user_setup.sh'."
